ff3d74b032d746e45b5eb1e76b26f0495b74ab32
zpeng11 <a2843173630@gmail.com>
2024-11-14 19:37:14 -0800

    finished RIFE export and quck test

--- RIFE/playground.py
+++ RIFE/playground.py
@@ -12,11 +12,15 @@ modulo = 128
 
 device = torch.device("cuda", 0)
 model_name = "flownet.pkl"
-export_name = './export/rife_256'
+
+#change these 3 params
+export_name = './export/rife_x4/rife_384'
+internal_size = 384
+sharpen_factor = 2
+
 
 image_size = 512
-internal_size = 256
-sharpen_factor = 1
+
 
 scale = 1.0
 ensemble = False
@@ -57,7 +61,9 @@ def init_module(
 
 pw = image_size
 ph = image_size
-timestep = torch.full([1, 1, ph, pw], 0.5, dtype=dtype, device=device)
+timestep1 = torch.full([1, 1, ph, pw], 1.0/4.0, dtype=dtype, device=device)
+timestep2 = torch.full([1, 1, ph, pw], 2.0/4.0, dtype=dtype, device=device)
+timestep3 = torch.full([1, 1, ph, pw], 3.0/4.0, dtype=dtype, device=device)
 tenFlow_div = torch.tensor([(pw - 1.0) / 2.0, (ph - 1.0) / 2.0], dtype=torch.float, device=device)
 tenHorizontal = torch.linspace(-1.0, 1.0, pw, dtype=torch.float, device=device)
 tenHorizontal = tenHorizontal.view(1, 1, 1, pw).expand(-1, -1, ph, -1)
@@ -68,12 +74,14 @@ backwarp_tenGrid = torch.cat([tenHorizontal, tenVertical], 1)
 
 
 class RIFEWrapped(nn.Module):
-    def __init__(self, timestep, tenFlow_div, backwarp_tenGrid, flownet, encoder):
+    def __init__(self, flownet, encoder):
         super().__init__()
         self.encoder = encoder
         self.flownet = flownet
 
-        self.timestep = torch.full([1, 1, internal_size, internal_size], 0.5, dtype=dtype, device=device)
+        self.timestep_1 = torch.full([1, 1, internal_size, internal_size], 1.0/4.0, dtype=dtype, device=device)
+        self.timestep_2 = torch.full([1, 1, internal_size, internal_size], 2.0/4.0, dtype=dtype, device=device)
+        self.timestep_3 = torch.full([1, 1, internal_size, internal_size], 3.0/4.0, dtype=dtype, device=device)
         self.tenFlow_div = torch.tensor([(internal_size - 1.0) / 2.0, (internal_size - 1.0) / 2.0], dtype=torch.float, device=device)
         tenHorizontal = torch.linspace(-1.0, 1.0, internal_size, dtype=torch.float, device=device)
         tenHorizontal = tenHorizontal.view(1, 1, 1, internal_size).expand(-1, -1, internal_size, -1)
@@ -91,15 +99,22 @@ class RIFEWrapped(nn.Module):
         encoded_0 = self.encoder(img_0)
         encoded_1 = self.encoder(img_1)
 
-        res = self.flownet(img_0, img_1, self.timestep, self.tenFlow_div, self.backwarp_tenGrid, encoded_0, encoded_1)
+        res1 = self.flownet(img_0, img_1, self.timestep_1, self.tenFlow_div, self.backwarp_tenGrid, encoded_0, encoded_1)
+        res2 = self.flownet(img_0, img_1, self.timestep_2, self.tenFlow_div, self.backwarp_tenGrid, encoded_0, encoded_1)
+        res3 = self.flownet(img_0, img_1, self.timestep_3, self.tenFlow_div, self.backwarp_tenGrid, encoded_0, encoded_1)
 
         if internal_size != image_size:
-            res = F.interpolate(res, size=(image_size, image_size), mode='bilinear', align_corners=False)
-            # res = adjust_sharpness(res, 1)
-        return res 
+            res1 = F.interpolate(res1, size=(image_size, image_size), mode='bilinear', align_corners=False)
+            res2 = F.interpolate(res2, size=(image_size, image_size), mode='bilinear', align_corners=False)
+            res3 = F.interpolate(res3, size=(image_size, image_size), mode='bilinear', align_corners=False)
+            if sharpen_factor!=1.0:
+                res1 = adjust_sharpness(res1, sharpen_factor)
+                res2 = adjust_sharpness(res2, sharpen_factor)
+                res3 = adjust_sharpness(res3, sharpen_factor)
+        return [res1, res2, res3]
     
 flownet, encoder = init_module(model_name, IFNet, scale, ensemble, device, dtype, Head)
-rife = RIFEWrapped(timestep, tenFlow_div, backwarp_tenGrid, flownet, encoder).eval()
+rife = RIFEWrapped(flownet, encoder).eval()
 
 
 
@@ -111,17 +126,20 @@ for i in range(10):
     ec0 = encoder(img_0)
     ec1 = encoder(img_1)
     
-    ref_output = flownet(img_0, img_1, timestep, tenFlow_div, backwarp_tenGrid, ec0, ec1).cpu().detach().numpy()
-    wrapped_output  = rife(img_0, img_1).cpu().detach().numpy()
+    ref_output_1 = flownet(img_0, img_1, timestep1, tenFlow_div, backwarp_tenGrid, ec0, ec1).cpu().detach().numpy()
+    ref_output_2 = flownet(img_0, img_1, timestep2, tenFlow_div, backwarp_tenGrid, ec0, ec1).cpu().detach().numpy()
+    ref_output_3 = flownet(img_0, img_1, timestep3, tenFlow_div, backwarp_tenGrid, ec0, ec1).cpu().detach().numpy()
+    wrapped_output  = rife(img_0, img_1)
     
-    print(wrapped_output.mean(), wrapped_output.max(), ref_output.mean(), ref_output.max())
-    print("MSE is: ",((ref_output - wrapped_output) ** 2).mean())
+    print("1st MSE is: ",((ref_output_1 - wrapped_output[0].cpu().detach().numpy()) ** 2).mean())
+    print("2nd MSE is: ",((ref_output_2 - wrapped_output[1].cpu().detach().numpy()) ** 2).mean())
+    print("2nd MSE is: ",((ref_output_3 - wrapped_output[2].cpu().detach().numpy()) ** 2).mean())
 
 
 img_0 = torch.rand((1,3,image_size,image_size), dtype=dtype, device=device)
 img_1 = torch.rand((1,3,image_size,image_size), dtype=dtype, device=device)
 input_list = ['img_0', 'img_1']
-output_list = ['result']
+output_list = ['result_1', 'result_2', 'result_3']
 input_tuple = (img_0, img_1)
 
 import onnx
@@ -157,12 +175,16 @@ for i in range(20):
     ec0 = encoder(img_0)
     ec1 = encoder(img_1)
 
-    wrapped_output  = flownet(img_0, img_1, timestep, tenFlow_div, backwarp_tenGrid, ec0, ec1).cpu().detach().numpy()
+    ref_output_1  = flownet(img_0, img_1, timestep1, tenFlow_div, backwarp_tenGrid, ec0, ec1).cpu().detach().numpy()
+    ref_output_2  = flownet(img_0, img_1, timestep2, tenFlow_div, backwarp_tenGrid, ec0, ec1).cpu().detach().numpy()
+    ref_output_3  = flownet(img_0, img_1, timestep3, tenFlow_div, backwarp_tenGrid, ec0, ec1).cpu().detach().numpy()
 
-    trt_res = rife_proc.inference([img_0.cpu().detach().numpy(), img_1.cpu().detach().numpy()])[0]
+    trt_res = rife_proc.inference([img_0.cpu().detach().numpy(), img_1.cpu().detach().numpy()])
     
     # print(wrapped_output.mean(), wrapped_output.max(), trt_res.mean(), trt_res.max())
-    print("MSE is: ",((trt_res - wrapped_output) ** 2).mean())
+    print("1st MSE is: ",((trt_res[0] - ref_output_1) ** 2).mean())
+    print("1st MSE is: ",((trt_res[1] - ref_output_2) ** 2).mean())
+    print("1st MSE is: ",((trt_res[2] - ref_output_3) ** 2).mean())
 
 
 from tqdm import tqdm
